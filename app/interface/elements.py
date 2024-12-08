b46dedcdbc494bb603b716880460db170e28ec7e
KuzIlya <KuznetsovIlyaDM@yandex.ru>
2024-12-09 01:29:06 +0300

    ver 1.0

--- app/interface/elements.py
+++ app/interface/elements.py
@@ -1,7 +1,5 @@
 import PySimpleGUI as sg
 
-from shops_info import SHOPS
-
 from .colors import (
     BLUE_COLOR,
     WHITE_COLOR,
@@ -10,7 +8,7 @@ from .colors import (
     LIGHT_BLUE_COLOR
 )
 
-MARKINES_IMAGE_BASE64 = b'iVBORw0KGgoAAAANSUhEUgAAAHUAAAAOCAYAAAAR4VO+AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAASKSURBVHgB7VhLbttIEH2UgpnNLDQncM8JLCfAALOycgJ7doOxB6ZvYJ9A9AmcOYEYJAiyC30C0asAARzJJzB9gjhAFgGCiKliN81is5siEyfZ+AEFsZvV39f1qingHj8Vr/+BWuxihDtEQDYR5bRDG2WMsSS7afGddOybFzVGN2TG2sZy+fjGKNegUK3rxtTLfmWdC7J/p++bfzEeDsnnM8bBANtmvJFsk+vfy88rJH++KPoYC59OYFJzUT4me9LizwPMxSCP4SdrShaJcptvSDZDd0RkJ1adXMchWSzKu2SnqEhj8IYdizmx/4F5PkdFZr5m3BIhqjXctr/YxzTQ4yv0ICcPcPzoWcFFRraBHhhYZV74gcdXkb3qODH2jay6GXBnMhOhHplt8+BD+Ar1KGQyt9BNmexxr1A/HF4QoVEQIMyZ5KBF0fhdjjMmkg7A4+ABfjeEfhUeOOpi8/tU1CnozVHohrmjTkFH77HjXQodyesQozq1E7STcmTGkwfpf2hi2lLGOiiyBfQ64jbHIMeYCD179LyYy9GC5Hc1QEj1xZxIglN8Qrr1sp4qOM+SVCsjvxI85tIzp7AsuEhl8Cm5NB1wgz6EMv4QzxNUJPPiztAkgzc5w92BFUeSyXIYoX9k+sB9s/JwXmQ5zlxOeY6EInX2dh+b+QpPP66Q/vWi2IMailwb0D4F2A70fo04atEkcBN+tUvLB5vUBFr/uSETwblJ5iKOXp88A9aJEchEH7wZLH0yWnbRL6eWffogF56imwp0AffD81SmHEKT8LfTe1iQkJG0XgdDTH8dFASnRDYfbBDhO6guQpcU1WnAZA6RPIydahKiI3JhE+gozR228PhLzD1tbTt1TDbvYa580+Z/hfW361j4p55+FTQBrj1a2O3f7iGhvHo718U+JkRqTLZkcvkd1y3CevQZ+S3nm6Hf3uQu+T0yEz8QddzxutMeon79v3T4bIsxXDJc4tCMyeSPrXkcYr2Mnph2O6asoDc9RotcdgSvjefP0jhFFbWNQ5NTzqT8eUrkbWBF6/1AEZg0o40JvNgr9m6TIrVQSiG/pXIC/k9IbjsvC76cypPeNBPNoAltu1wo6AWW8F0i5qiIn6EpwyVSMy6/j0TfyrQ7QfslJTPtQtQ3PjTjx/B/mnRFbOY5g/8mvkF5MiNi35OkTvPfMCOCE4qn8+KtyKFk1xRmSSG/dHl6WF2eElSXQx+pjTzrk9ORmbTq4B+LurbcOLbaR6Y+RFPmJPj9O8tnZvnJd6GoZx+XXF5BRwEca0g9/Sq4EVl+RXsjv1HpZMnv0sjvri2//A8TS7ApZvgK+T0X/d1YzxM0ce7wUaK+LQLKD/5yM7ntyPTjmwcjht6oWNTxDTsyllntM+uZlecJmtFdpprY6kPeOtvmVSIyfcSy/a387mGD5PRs63kRdandmAm8+I/2hP9pGmKHLlLqF72PkekrQw8EuMd3BUdizoeYb7or3PANlwkmshU4hw7oYOvna/pN6SAsB5+Q2N+ufXBP6g9EjWDKs5xDBwFFouMPiG/BF7A71mLQL22LAAAAAElFTkSuQmCC'
+from .images_base64 import MARKINES_IMAGE_BASE64
 
 TITLE_BAR = sg.Titlebar(
     'Markines Repricer',
@@ -22,7 +20,7 @@ TITLE_BAR = sg.Titlebar(
 
 TOP_NAME = sg.Text(
     'REPRICER',
-    font=('Anonymous Pro', 25),
+    font=('Anonymous Pro', 25, 'bold'),
     expand_x=True,
     justification='c',
     background_color=WHITE_COLOR,
@@ -31,16 +29,21 @@ TOP_NAME = sg.Text(
 
 
 FOOTER_IMAGE = [
-    sg.Column(
+    sg.Frame(
+        '',
         [
             [
                 sg.Image(
                     data=MARKINES_IMAGE_BASE64,
                     background_color=WHITE_COLOR,
+                    expand_y=True,
+                    pad=((330, 0), (0, 0))
                 )
             ]
         ],
-        justification='center',
+        vertical_alignment='b',
+        background_color=WHITE_COLOR,
+        border_width=0
     )
 ]
 
@@ -52,7 +55,7 @@ def get_window(
         'Window Title',
         layout,
         size=(800, 500),
-        background_color=WHITE_COLOR
+        background_color=WHITE_COLOR,
     )
 
 
@@ -66,17 +69,24 @@ def get_shop_frame(name: str) -> sg.Frame:
                 justification='l',
                 expand_x=True
             ),
-            sg.Button(
+            sg.FileBrowse(
                 'Загрузить',
+                enable_events=True,
                 button_color=BUTTON_COLOR,
-                mouseover_colors=LIGHT_BLUE_COLOR,
-                border_width=0,
+                file_types=[
+                    ('xlsx files', '*.xlsx')
+                ],
+                target=('ADD_FILE' + name),
+                key=('ADD_FILE' + name)
             ),
             sg.Button(
                 'Обновить',
                 button_color=BUTTON_COLOR,
                 mouseover_colors=LIGHT_BLUE_COLOR,
                 border_width=0,
+                disabled=True,
+                target=('REFRESH' + name),
+                key=('REFRESH' + name),
             ),
         ]
     ]
@@ -87,12 +97,13 @@ def get_shop_frame(name: str) -> sg.Frame:
         border_width=0,
         background_color=BLUE_COLOR,
         expand_x=True,
+        pad=((45, 45), (2, 2)),
         relief=sg.RELIEF_FLAT
     )
 
 
-def get_shop_frames() -> list[sg.Frame]:
+def get_shop_frames(shops: dict) -> list[sg.Frame]:
     result_layout = []
-    for shop in SHOPS:
+    for shop in shops:
         result_layout.append([get_shop_frame(shop)])
     return result_layout
---
+++ main.py
@@ -0,0 +1,44 @@
+import PySimpleGUI as sg
+
+from app.interface.elements import (
+    TITLE_BAR,
+    TOP_NAME,
+    FOOTER_IMAGE,
+    get_shop_frames,
+    get_window
+)
+from app.events.utils import add_file, process_file
+
+SHOPS = {
+    'Тестовый магазин': 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjQxMDE2djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTc0NzQzNDk4NCwiaWQiOiIwMTkzMmY2NS0zZWFjLTdjYTktYjBkMC01ZmI1ZGFiOGRkYjEiLCJpaWQiOjEwMTc1NTI2OSwib2lkIjoxMTg2NjYzLCJzIjozODM4LCJzaWQiOiI5NmNlMGE2Yy0zYTNkLTQyYTItOWNhYS03YWVkOTBiMjVmY2YiLCJ0IjpmYWxzZSwidWlkIjoxMDE3NTUyNjl9._8EnoZLOqrdZA1R_EVAbcEKG_dTwTIYj61U7xqJXoBTxYhh4bHQLQXM-fu2na7y2f4UKLxVjpRDnU4nKL2I65g',
+}
+
+main_layout = [
+    [TITLE_BAR],
+    [TOP_NAME],
+]
+
+main_layout += get_shop_frames(SHOPS)
+
+main_layout += [FOOTER_IMAGE]
+
+file_path = {}
+
+window = get_window(main_layout)
+
+
+def main() -> None:
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED,):
+            break
+        if 'ADD_FILE' in event:
+            add_file(event, values, file_path, window)
+        if 'REFRESH' in event:
+            process_file(event, file_path, SHOPS, window)
+
+    window.close()
+
+
+if __name__ == '__main__':
+    main()
